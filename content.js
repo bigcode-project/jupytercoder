d7a1039fc1e7be48fc9e64ec63ddf2886fd2ab1a
Jia Li <jia@cardiologs.com>
2023-04-06 16:23:13 +0200

    insert suggestion with textarea

--- content.js
+++ content.js
@@ -12,19 +12,11 @@ if (document.querySelector('body.notebook_app')) {
         const cellContent = getCellContent(activeCell);
 
         // Log the cell content to the console
-        console.log(activeCell.CodeMirror);
+        console.log(activeCell);
 
         // Suggestion handling
         const suggestion = 'your_suggestion_here'; // Replace with your actual suggestion
-        handleSuggestion(activeCell.CodeMirror, suggestion);
-      }
-    }
-
-    // Check if the ArrowRight key was pressed
-    if (event.code === 'ArrowRight') {
-      const activeCell = document.querySelector('.cell.selected .input_area .CodeMirror');
-      if (activeCell) {
-        validateSuggestion(activeCell.CodeMirror);
+        insertSuggestion(activeCell, suggestion);
       }
     }
   });
@@ -41,22 +33,23 @@ if (document.querySelector('body.notebook_app')) {
   }
 }
 
-function handleSuggestion(cm, suggestion) {
-  const cursor = cm.getCursor();
-  cm.replaceRange(suggestion, cursor, null, '+suggest');
-  const endPos = cm.getCursor();
-  cm.markText(cursor, endPos, {
-    className: 'greyed-suggestion',
-    atomic: true,
-    inclusiveRight: true,
-  });
-}
+function insertSuggestion(cell, suggestion) {
+  // Get the textarea inside the div
+  const textarea = cell.querySelector('textarea');
 
-function validateSuggestion(cm) {
-  const marks = cm.getAllMarks();
-  if (marks.length) {
-    const mark = marks[0];
-    mark.clear();
-    cm.setCursor(cm.getCursor());
+  if (textarea) {
+    // Get the current cursor position
+    const cursorPosition = textarea.selectionStart;
+
+    // Insert the suggestion at the cursor position
+    const newValue = textarea.value.slice(0, cursorPosition) + suggestion + textarea.value.slice(cursorPosition);
+    textarea.value = newValue;
+
+    // Update the cursor position after inserting the suggestion
+    textarea.selectionStart = textarea.selectionEnd = cursorPosition + suggestion.length;
+
+    // Trigger a change event on the textarea to update the CodeMirror instance
+    const event = new Event('input', { bubbles: true, cancelable: true });
+    textarea.dispatchEvent(event);
   }
 }
