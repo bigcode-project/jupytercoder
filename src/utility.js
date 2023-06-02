53268bd543f174824e03f52c85bd89449af9a4f7
ran <redamancy0326@foxmail.com>
2023-06-02 11:01:43 +0800

    add commont

--- src/utility.js
+++ src/utility.js
@@ -575,7 +575,8 @@ utility.insertSuggestionFixBug = (suggestion, activeRequestTextarea, currctJupyt
         if (cellContent[index].type == "output"){
             continue
         }
-        for (let j = 0; j < cellContent[index].content.length + cellContent.length*3; j++) {
+        // cellContent.length*3 => Each line of jupyter may have empty values, occupying three lengths，But it may not necessarily appear, as this code generally deletes all the code within the cell, so there is no difference in usage
+        for (let j = 0; j < cellContent[index].content.length + cellContent.length * 3; j++) {
             simulateUserPressingMoveMouseRight(activeRequestTextarea)
             simulateUserPressingBackspace(activeRequestTextarea)
         }
